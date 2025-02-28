c77fd6024207906b4eaa4168bff9a26a89938ff8
PSCEMateos <emateos@pscworld.com.mx>
2025-02-28 17:03:10 -0600

    Add files via upload

---
+++ Modelos/RecivedMedia/EncryptedRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WhatsAppPresentacionV3.Modelos.RecivedMedia
+{
+    public class EncryptedRequestModel
+    {
+        public string encrypted_flow_data { get; set; }
+        public string encrypted_aes_key { get; set; }
+        public string initial_vector { get; set; }
+    }
+}
