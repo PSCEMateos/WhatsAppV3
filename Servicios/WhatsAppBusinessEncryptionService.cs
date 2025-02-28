3c0cf75b429774d907fd44fcf45a2f037ae54d2f
PSCEMateos <emateos@pscworld.com.mx>
2025-02-28 17:00:12 -0600

    Update WhatsAppBusinessEncryptionService.cs

--- Servicios/WhatsAppBusinessEncryptionService.cs
+++ Servicios/WhatsAppBusinessEncryptionService.cs
@@ -1,17 +1,19 @@
-﻿using System.Net.Http.Headers;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace WhatsAppPresentacionV3.Servicios
 {
-    public class WhatsAppBusinessEncryptionService : WhatsAppMessageService
+    public class WhatsAppBusinessEncryptionService
     {
 
         private readonly string _tokenAcceso;
         private readonly string _facebookGraphVersion = "v21.0";
         //private readonly string _WHATSAPP_BUSINESS_ACCOUNT_ID;
         private readonly string _idTelefono;
-        public WhatsAppBusinessEncryptionService(string idTelefono, string tokenAcceso) : base(idTelefono, tokenAcceso)
+        public WhatsAppBusinessEncryptionService(string idTelefono, string tokenAcceso)
         {
             _tokenAcceso = tokenAcceso;
             _idTelefono = idTelefono;
@@ -20,12 +22,23 @@ namespace WhatsAppPresentacionV3.Servicios
             $"https://graph.facebook.com/{_facebookGraphVersion}/{_idTelefono}/whatsapp_business_encryption";
         public async Task<string> UploadPublicKeyAsync(string publicKey)
         {
-            var payload = new
-            {
-                encryption_key = publicKey.Replace("\n", "").Replace("\r", "")
-            };
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _tokenAcceso);
 
-            return await EnviarMensaje(BuildApiUrl(), payload);
+            var payload = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("business_public_key", publicKey) //encryption_key = publicKey.Replace("\n", "").Replace("\r", "")
+            });
+
+            HttpResponseMessage response = await client.PostAsync(BuildApiUrl(), payload);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error al subir la clave pública: {responseBody}");
+            }
+
+            return $"Clave pública subida correctamente: {responseBody}";
         }
     }
 }
